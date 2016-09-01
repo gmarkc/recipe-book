71b68a60a422814818ecdad8aa358c9b127edcfe
gmarkc <greg.cockerham@gmail.com>
2016-09-01 11:21:19 -0400

    Adding to GitHub repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule  } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent, HeaderComponent } from './';
+import { RecipesComponent } from './recipes';
+import { RecipeListComponent, RecipeItemComponent } from './recipes/recipe-list';
+import { RecipeDetailComponent } from './recipes/recipe-detail';
+import { ShoppingListComponent, ShoppingListAddComponent } from './shopping-list';
+
+@NgModule({
+    declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeItemComponent, RecipeDetailComponent, ShoppingListComponent, ShoppingListAddComponent],
+    imports: [BrowserModule, FormsModule],
+    bootstrap: [AppComponent] 
+})
+export class AppModule {}
